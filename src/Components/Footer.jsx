9df74257e7754f4cbdf149c81a3bf2cb7a2a8a46
Kokai Laszlo <kokai.laszlo@gmail.com>
2025-04-03 01:33:05 -0400

    event planner

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+        <footer>
+            <div className="copyright">
+                &copy; 2025 Kókai László
+            </div>
+        </footer>
+        </>
+    );
+};
+
+export default Footer;
